6c418b07f687ab489865c0a195c7541346803fb1
Red <alexandru.ionut.rosu@gmail.com>
2020-03-03 00:42:15 +0200

    Navigation js

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,2 +1,37 @@
 "use strict";
-console.log('dodel!!!!!');
+// ------------------------------------------------------------
+// Scroll to anchor
+// ------------------------------------------------------------
+function scrollToAnchor(target, offset) {
+    if (offset === void 0) { offset = 0; }
+    var distanceToTop = function (el) { return Math.floor(el.getBoundingClientRect().top); };
+    var targetAnchor = document.querySelector(target);
+    if (!targetAnchor)
+        return;
+    if (offset === null) {
+        offset = 0;
+    }
+    var originalTop = distanceToTop(targetAnchor) + parseInt(offset);
+    window.scrollBy({ top: originalTop, left: 0, behavior: 'smooth' });
+}
+function addNavigationListeners() {
+    var navigations = document.querySelectorAll('[data-role="navigation"]');
+    navigations.forEach(function (nav) {
+        nav.addEventListener('click', function (event) {
+            event.preventDefault();
+            var eventTarget = event.target;
+            var targetValue = eventTarget.getAttribute('data-target');
+            var targetOffset = eventTarget.getAttribute('data-offset');
+            var targetLocation = "[data-location=\"" + targetValue + "\"]";
+            if (!!targetValue) {
+                var offset = targetOffset ? targetOffset : 0;
+                console.log(offset);
+                scrollToAnchor(targetLocation, offset);
+            }
+        });
+    });
+}
+// Initialize event listeners
+document.addEventListener('DOMContentLoaded', function () {
+    addNavigationListeners();
+});
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,1 +1,51 @@
-console.log('dodel!!!!!');
+// ------------------------------------------------------------
+// Scroll to anchor
+// ------------------------------------------------------------
+
+
+
+function scrollToAnchor( target:string, offset:any=0 ) {
+    const distanceToTop = ( el:HTMLElement ) => Math.floor(el.getBoundingClientRect().top);
+    const targetAnchor = document.querySelector( target );
+    if ( !targetAnchor ) return;
+    if ( offset === null ) {
+        offset = 0;
+    }
+    const originalTop = distanceToTop(targetAnchor as HTMLElement) + parseInt(offset);
+
+    window.scrollBy( { top: originalTop, left: 0, behavior: 'smooth' } );
+}
+
+
+
+
+function addNavigationListeners() {
+    const navigations = document.querySelectorAll( '[data-role="navigation"]' );
+
+    navigations.forEach( (nav) => {
+        nav.addEventListener( 'click', (event) => {
+            event.preventDefault();
+
+            const eventTarget = event.target as HTMLElement;
+            const targetValue = eventTarget.getAttribute( 'data-target' );
+            const targetOffset = eventTarget.getAttribute( 'data-offset' );
+            const targetLocation = `[data-location="${targetValue}"]`;
+
+            if ( !!targetValue ) {
+                const offset = targetOffset ? targetOffset : 0;
+                console.log(offset);
+                scrollToAnchor( targetLocation, offset );
+            }
+        } );
+    } );
+}
+
+
+
+
+// Initialize event listeners
+
+document.addEventListener( 'DOMContentLoaded', () => {
+    addNavigationListeners();
+} );
+
